c076b4945ded499e1347941d53aa3b5ba3d2d040
Sawsan Attyani <sattyani@lcs.on.ca>
2019-11-28 14:28:45 -0500

    Initial Commit

---
+++ SnowCalls/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnowCalls
+//
+//  Created by Attyani, Sawsan on 2019-11-28.
+//  Copyright © 2019 Attyani, Sawsan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
